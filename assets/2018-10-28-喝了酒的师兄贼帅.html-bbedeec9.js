0f31fe9afe87bd3c36d7ed74e5af2ee068e6ede8
Wang-Alan <whl951216@outlook.com>
2023-08-16 10:57:44 +0000

    Deploying to gh-pages from @ Wang-Alan/myblog@ab0536218d1338b63192e43477f5a63f4cdab7b6 🚀

---
+++ assets/2018-10-28-喝了酒的师兄贼帅.html-bbedeec9.js
@@ -0,0 +1,1 @@
+import{_}from"./plugin-vue_export-helper-c27b6911.js";import{o as e,c as t,a as o}from"./app-960c75cd.js";const c={},s=o("p",null,"酒是个好东西呀！",-1),n=[s];function r(a,l){return e(),t("div",null,n)}const m=_(c,[["render",r],["__file","2018-10-28-喝了酒的师兄贼帅.html.vue"]]);export{m as default};
